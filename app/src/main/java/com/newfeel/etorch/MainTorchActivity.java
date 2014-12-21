6e1b2891ec74c70d573cac9feff735566b35088b
El bani Naoufal <elbaninaoufal@gmail.com>
2014-12-21 15:04:36 +0100

    deleting unused methods.

--- app/src/main/java/com/newfeel/etorch/MainTorchActivity.java
+++ app/src/main/java/com/newfeel/etorch/MainTorchActivity.java
@@ -3,8 +3,6 @@ package com.newfeel.etorch;
 import android.content.Context;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.view.View;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -36,19 +34,19 @@ public class MainTorchActivity extends ActionBarActivity {
 
         // check if flash is supported
         if (!light.supportFLash()) {
-            Toast.makeText(context ,"Error: Your device does not support flash light!", Toast.LENGTH_LONG).show();
+            Toast.makeText(context, "Error: Your device does not support flash light!", Toast.LENGTH_LONG).show();
         }
 
         switchLight.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
-                try{
-                    if(light.getStatus() == light.LIGHT_OFF) {
+                try {
+                    if (light.getStatus() == light.LIGHT_OFF) {
                         light.lightOn();
                     } else {
                         light.lightOff();
                     }
-                } catch(Exception e) {
-                    Toast.makeText(context ,"Error: " + e.getMessage(), Toast.LENGTH_LONG).show();
+                } catch (Exception e) {
+                    Toast.makeText(context, "Error: " + e.getMessage(), Toast.LENGTH_LONG).show();
                 }
             }
         });
@@ -67,27 +65,5 @@ public class MainTorchActivity extends ActionBarActivity {
         light = new Light(context);
 
         super.onResume();
-    }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main_torch, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
     }
 }
